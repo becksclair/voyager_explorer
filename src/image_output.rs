615bef1a11a881aa152b0a03f443c6f631fed61d
Rebecca Clair <rebecca@heliasar.com>
2025-05-29 21:01:42 +0200

    feat: Add egui dependency and update image output logic

--- src/image_output.rs
+++ src/image_output.rs
@@ -1,12 +1,12 @@
-use eframe::egui::{Color32, ColorImage};
+use egui::ColorImage;
 
 pub fn image_from_pixels(pixels: &[u8]) -> ColorImage {
     let width = 512;
     let height = pixels.len() / width;
 
-    let mut img = ColorImage::new([width, height], Color32::BLACK);
+    let mut img = ColorImage::new([width, height], egui::Color32::BLACK);
     for (i, p) in pixels.iter().enumerate() {
-        img.pixels[i] = Color32::from_gray(*p);
+        img.pixels[i] = egui::Color32::from_gray(*p);
     }
 
     img
