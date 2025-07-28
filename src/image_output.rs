11a1aec07dadf438cfca52c8ae4e5c1ce5bfbf74
Rebecca Clair <rebecca@heliasar.com>
2025-07-28 21:37:25 +0200

    fix: bugfix

--- src/image_output.rs
+++ src/image_output.rs
@@ -3,7 +3,7 @@ use egui::ColorImage;
 pub fn image_from_pixels(pixels: &[u8]) -> ColorImage {
     let width = 512;
     let height = pixels.len() / width;
-    let mut img = ColorImage::new([width, height], egui::Color32::BLACK);
+    let mut img = ColorImage::new([width, height], vec![egui::Color32::BLACK; width * height]);
 
     let pixel_count = width * height;
     for (i, p) in pixels.iter().take(pixel_count).enumerate() {
